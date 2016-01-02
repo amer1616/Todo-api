e212c02d9c4f11b7b57be0886482e9b258e923ed
AR <amer1616@yahoo.com>
2016-01-02 18:02:16 +0300

    Add POST for todos

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 var express = require('express');
+//parsing json body ..to be parsed by express
+var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//create todos obj model
-var todos = [
-{
-  id: 1,
-  description: 'Meet mom for launch',
-  completed: false
-},
-{
-  id: 2,
-  description: 'Go to Market',
-  completed: false
-},
-{
-  id: 3,
-  description: 'Feed the cat',
-  completed: false
-}];
+//create todos obj model of array
+var todos = [];
 
+//create obj to keep tracking our data.. to increment new todo id so getting new id
+var todoNextId = 1;
+
+//adding bodyParser for parsing json from the server by express
+app.use(bodyParser.json());
 
 app.get('/', function (req,res){
-  res.send('Todo API Root');
+	res.send('Todo API Root');
 });
 
 //GET /todos
 app.get('/todos', function (req, res){
-  res.json(todos);
+	res.json(todos);
 });
 
 //geting single todos by fetching id ... GET /todos/:id
@@ -39,20 +31,34 @@ app.get('/todos/:id', function (req, res){
   var matchedid;
 
   todos.forEach(function (todo){
-    if (todoid === todo.id){
-      matchedid = todo;
-    }
-  })
+  	if (todoid === todo.id){
+  		matchedid = todo;
+  	}
+  });
+
   if (matchedid){
-    res.json(matchedid);
+  	res.json(matchedid);
   }else{
-    res.status(404).send();
+  	res.status(404).send();
   }
+});
 
+//POST /todos/:id
+app.post('/todos', function (req, res){
+	var body = req.body;
 
+	//add id field
+	body.id = todoNextId++;
+
+	//push body into array
+	todos.push(body);
+
+	//console.log('description: ' + body.description);
+
+	res.json(body);
 });
 
 
 app.listen(PORT, function (){
-  console.log('Express is listening on port: ' + PORT);
-})
+	console.log('Express is listening on port: ' + PORT);
+});
