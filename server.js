e2f77cc009ee8920d57c634347b7365d50088812
AR <amer1616@yahoo.com>
2016-01-04 19:29:34 +0300

    Add DELETE todos

--- server.js
+++ server.js
@@ -65,6 +65,21 @@ app.post('/todos', function (req, res){
 	res.json(body);
 });
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res){
+	//fetching the matched todoid by using _.findwhere
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	//check if matchedTodo not found return error 404, else set todos without the matchedTodo id & respond with matchedTodo id in json
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todo found with that id"});
+	}else{
+
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 
 app.listen(PORT, function (){
 	console.log('Express is listening on port: ' + PORT);
